10cb0aab6ef6260371340d6f656ca04d8b18a0c9
“Your <echosmania@gmail.com>
2023-01-27 05:53:40 -0300

    Change scrape method using market name and partner id

--- index.js
+++ index.js
@@ -1,46 +1,26 @@
 import { firefox } from 'playwright'
 import dotenv from 'dotenv'
 import { saveMarketStatic } from './db/local.js'
+import { randomBetween } from './utils/utils.js'
 
 dotenv.config()
 
-const ScrapeData = async (browser, url) => {
-  const result = []
+const ScrapeData = async (browser, { marketName, partnerId }) => {
+  const page = await browser.newPage()
+  const baseUrl = process.env.BASE_URL
+  await page.goto(baseUrl)
 
-  let page = await browser.newPage()
-  page.goto(url)
-
-  let response
-  try {
-    response = await page.waitForResponse('**/v3/catalogues/**', { timeout: 3000 })
-  } catch {
-    try {
-      await page.close()
-      page = await browser.newPage()
-      page.goto(url)
-
-      response = await page.waitForResponse('**/v3/catalogues/**', { timeout: 3000 })
-    } catch {
-      console.log('Failed to load url:', url)
-      result.push(`Failed to load url: ${url}`)
-      return
-    }
-  }
-
-  const rawCategoriesUrl = await response.url()
-  const categoriesUrl = rawCategoriesUrl.split('&')[0] + '&max=1000&maxProducts=10&offset=0'
-  const partnerId = rawCategoriesUrl.split('partnerId=')[1].split('&')[0]
-
-  await page.waitForSelector('h1')
-  const h1El = await page.$('h1')
-  const marketName = await h1El.textContent()
+  const categoryUrlSplit = process.env.PEDIDOSYA_CATEGORY_URL.split('${}')
+  const categoriesUrl =
+    categoryUrlSplit[0] + randomBetween(100000, 999999) + categoryUrlSplit[1] + partnerId + categoryUrlSplit[2]
 
   console.log(marketName)
 
   const productLink = process.env.PEDIDOSYA_PRODUCT_URL.split('${}')
+
   const categories = await page.evaluate(
     async ({ categoriesUrl, partnerId, marketName, productLink }) => {
-      const getCategoryUrl = (productId, partnerId) => (productLink[0] + productId + productLink[1] + partnerId + productLink[2])
+      const getCategoryUrl = (productId, partnerId) => productLink[0] + productId + productLink[1] + partnerId + productLink[2]
 
       const response = await fetch(categoriesUrl)
       const categories = await response.json()
@@ -52,6 +32,7 @@ const ScrapeData = async (browser, url) => {
 
         const response = await fetch(getCategoryUrl(categoryId, partnerId))
         const productsData = await response.json()
+        console.log('products data', productsData)
         const products = productsData.data.map((d) => {
           return {
             image: d.image,
@@ -85,22 +66,18 @@ const ScrapeData = async (browser, url) => {
     await saveMarketStatic(category, i + 1)
   }
 
-  await page.close()
-  return result.length === 0 ? [`All scraped without errors on ${marketName}`] : result
+  return await page.close()
 }
 
 ;(async () => {
   const browser = await firefox.launch()
   const initalTime = Date.now()
 
-  const marketURLs = process.env.MARKET_URLS.split(' ')
-  const results = []
-  for (const url of marketURLs) {
-    const result = await ScrapeData(browser, url)
-    results.push(result)
+  const dataMarkets = JSON.parse(process.env.DATA_MARKETS)
+  console.log(dataMarkets)
+  for (const data of dataMarkets) {
+    await ScrapeData(browser, data)
   }
-
-  console.log('results: ', results)
 
   await browser.close()
 
