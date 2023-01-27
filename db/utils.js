09abe40fb793bd8af2191d12a7f5fa8d7d462633
“Your <echosmania@gmail.com>
2023-01-27 05:54:57 -0300

    Added path to prevent linux/windows/mac diff problems

--- db/utils.js
+++ db/utils.js
@@ -16,9 +16,9 @@ const sortByDate = (prices) => {
   return [...prices].sort((a, b) => a.date.getTime() > b.date.getTime())
 }
 
-const write = (fileName, content) => writeFileSync(`${DB_PATH}/${fileName}.json`, JSON.stringify(content, null, 2), 'utf-8')
+const write = (fileName, content) => writeFileSync(path.join(DB_PATH, `${fileName}.json`), JSON.stringify(content, null, 2), 'utf-8')
 const read = (fileName) => {
-  const file = readFileSync(`${DB_PATH}/${fileName}.json`, 'utf-8')
+  const file = readFileSync(path.join(DB_PATH, `${fileName}.json`), 'utf-8')
   return JSON.parse(file)
 }
 
