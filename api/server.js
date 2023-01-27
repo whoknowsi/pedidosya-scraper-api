57a3878f1c6c40916e4c20b4c30111ef34d633d1
“Your <echosmania@gmail.com>
2023-01-27 06:11:34 -0300

    Added products endpoint with pagination

--- api/server.js
+++ api/server.js
@@ -1,8 +1,25 @@
 import { Hono } from 'hono'
 import { serveStatic } from 'hono/serve-static.module'
+import products from '../assets/static/db/products.json'
 const app = new Hono()
 
 app.get('/', (c) => c.text('Hono!!'))
+
+app.get('/products', (c) => {
+  const { offset = 0, limit = 10 } = c.req.queries
+  const productsToSend = products.slice(offset, offset + limit)
+
+  c.json({
+    products: productsToSend,
+    offset,
+    limit,
+    count: productsToSend.length,
+    max: products.length,
+    prev: offset ? `/products?limit=${limit}&offset${Math.max(0, offset - limit)}` : null,
+    next: products.length > (offset + 1) ? `/products?limit=${limit}&offset${offset + limit}` : null
+  })
+})
+
 app.get('/static/*', serveStatic({ root: './' }))
 
 export default app
