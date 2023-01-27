c47f49710517112475c3a809d5402cc1fe006d4e
“Your <echosmania@gmail.com>
2023-01-27 08:09:01 -0300

    Add filter by market and fix prev and next

--- api/server.js
+++ api/server.js
@@ -1,22 +1,39 @@
 import { Hono } from 'hono'
 import { serveStatic } from 'hono/serve-static.module'
 import products from '../assets/static/db/products.json'
+import categories from '../assets/static/db/categories.json'
+import markets from '../assets/static/db/markets.json'
 const app = new Hono()
 
 app.get('/', (c) => c.text('Hono!!'))
 
 app.get('/products', (c) => {
-  const { offset = 0, limit = 10 } = c.req.queries
-  const productsToSend = products.slice(offset, offset + limit)
+  const { offset = 0, limit = 10, marketId } = c.req.queries
+
+  let max = products.length
+  let productsToSend = products
+
+  if (marketId) {
+    const productsIdByMarket = markets.find((market) => market.id === marketId)?.products || []
+    max = productsIdByMarket.length
+    productsToSend = productsToSend
+      .filter((product) => productsIdByMarket.includes(product.id))
+      .map((product) => {
+        const { prices, ...prod } = product
+        return { ...prod, price: prices.find((price) => price.market === marketId)?.price || null }
+      })
+  }
+
+  productsToSend = productsToSend.slice(offset, offset + limit)
 
   c.json({
     products: productsToSend,
     offset,
     limit,
     count: productsToSend.length,
-    max: products.length,
-    prev: offset ? `/products?limit=${limit}&offset${Math.max(0, offset - limit)}` : null,
-    next: products.length > (offset + 1) ? `/products?limit=${limit}&offset${offset + limit}` : null
+    max,
+    prev: max > offset ? `/products?limit=${limit}&offset=${Math.max(0, offset - limit)}${marketId ? `&marketId=${marketId}` : ''}` : null,
+    next: max > (offset + limit) ? `/products?limit=${limit}&offset=${offset + limit}${marketId ? `&marketId=${marketId}` : ''}` : null
   })
 })
 
