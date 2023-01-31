7d30a4c7b8c459008db8d0208f3d60f34693957d
“Your <echosmania@gmail.com>
2023-01-31 18:17:25 -0300

    Parse barcode to fill always to 13 chars

--- utils/utils.js
+++ utils/utils.js
@@ -1,8 +1,17 @@
 export const randomBetween = (min, max) => Math.floor(Math.random() * (max - min) + min)
 
-const parseBarcode = (barcode) => {
-  if (barcode.length > 13) return barcode.replace(/^0/, '')
-  return barcode
+const parseBarcode = (string) => {
+  const length = string.length
+  if (length >= 13) {
+    let i = 0
+    while (string[i] === '0' && length - i > 13) {
+      i++
+    }
+    return string.slice(i)
+  } else {
+    const zeros = '0'.repeat(13 - length)
+    return zeros + string
+  }
 }
 
 const parseProducts = (products) => {
