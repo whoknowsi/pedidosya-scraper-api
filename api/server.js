fc8c383bdf0ae2176ec345ceb5c85ca9cda5d737
“Your <echosmania@gmail.com>
2023-02-13 21:48:17 -0300

    Improving cpu usage

--- api/server.js
+++ api/server.js
@@ -13,48 +13,7 @@ app.get('/products', (c) => {
   offset = offset ? Number(offset) : 0
   limit = limit ? Number(limit) : 10
 
-  let productsToSend = productsData.map(({ prices, ...product }) => {
-    const filledMarketsPrices = prices.map(({ market: id, ...price }) => {
-      return {
-        ...price,
-        market: {
-          id,
-          name: marketsData.find((market) => market.id === id)?.name || null,
-          image: marketsData.find((market) => market.id === id)?.image || null
-        }
-      }
-    })
-
-    return {
-      ...product,
-      prices: filledMarketsPrices
-    }
-  })
-
-  if (marketId) {
-    const market = marketsData.find((market) => market.id === marketId)
-    const productsIdByMarket = market?.products || []
-    productsToSend = productsToSend
-      .filter((product) => productsIdByMarket.includes(product.id))
-      .map((product) => {
-        const { prices, categories, ...prod } = product
-        const category = categoriesData.find((category) => JSON.stringify(category.markets).includes(marketId))
-        return {
-          ...prod,
-          price: prices.find((price) => price.market === marketId)?.price || null,
-          category: {
-            id: category.id,
-            name: category.name
-          },
-          market: {
-            id: market.id,
-            name: market.name,
-            image: market.image
-          },
-          product: { ...prod, prices, categories }
-        }
-      })
-  }
+  let productsToSend = productsData
 
   if (categoryId) {
     const productsByCategory = categoriesData.find((category) => category.id === categoryId)?.products || []
@@ -62,9 +21,51 @@ app.get('/products', (c) => {
       .filter((product) => productsByCategory.includes(product.id))
   }
 
+  if (marketId) {
+    const market = marketsData.find((market) => market.id === marketId)
+    const productsIdByMarket = market?.products || []
+    productsToSend = productsToSend
+      .filter((product) => productsIdByMarket.includes(product.id))
+  }
+
   const max = productsToSend.length
 
-  productsToSend = productsToSend.slice(offset, offset + limit)
+  productsToSend = productsToSend.slice(offset, offset + limit).map((product) => {
+    product.prices = product.prices.map(({ market, ...price }) => {
+      return {
+        ...price,
+        market: market.name
+          ? market
+          : {
+              id: market,
+              name: marketsData.find(({ id }) => id === market)?.name || null,
+              image: marketsData.find(({ id }) => id === market)?.image || null
+            }
+      }
+    })
+
+    if (marketId) {
+      const market = marketsData.find((market) => market.id === marketId)
+      const { prices, categories, ...prod } = product
+      const category = categoriesData.find((category) => JSON.stringify(category.markets).includes(marketId))
+      return {
+        ...prod,
+        price: prices.find((price) => price.market === marketId)?.price || null,
+        category: {
+          id: category.id,
+          name: category.name
+        },
+        market: {
+          id: market.id,
+          name: market.name,
+          image: market.image
+        },
+        product: { ...prod, prices, categories }
+      }
+    }
+
+    return product
+  })
 
   const prev = max > offset && offset > 0
     ? `/products?limit=${limit}&offset=${Math.max(0, offset - limit)}${marketId ? `&marketId=${marketId}` : ''}${categoryId ? `&categoryId=${categoryId}` : ''}`
